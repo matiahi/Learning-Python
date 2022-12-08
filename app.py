3ccfb124ca624decfb11de57be30fec77b01cb54
nona.matiahi@gmail.com <nona.matiahi@gmail.com>
2022-12-08 12:39:52 -0500

    My first Commit

---
+++ app.py
@@ -0,0 +1,26 @@
+print("Nona Mohammadi")
+print('o----')
+print(' ||||')
+
+price = 10
+price = 20
+rating = 4.9
+name = 'Mosh'
+is_published = True
+
+print(price )
+
+full_name = 'John smith'
+age = 20
+is_new = True
+
+name = input('What is your name? ')
+print('Hi ' + name)
+
+your_name = input('What is your name? ')
+favorite_color = input('What is your favorite color? ')
+print(your_name + ' likes ' + favorite_color)
+
+birth_year = input('Birth year: ')
+age = 2022 - int(birth_year)
+print(age)
