f8daaaa0aa848684202396403ab410e9712119cf
Nona <48446884+matiahi@users.noreply.github.com>
2023-04-12 13:19:23 -0600

    Add files via upload

---
+++ If-else-exercise.py
@@ -0,0 +1,8 @@
+name = input('Please give me your name: ')
+
+if name == 'nona' or 'Nona':
+    print('Hello my friend!')
+elif name == 'jadi':
+    print('Hello Jadi!')
+else:
+    print('Hello Stranger!')
